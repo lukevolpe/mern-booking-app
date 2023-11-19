9620d20d5557fdab7882cb0fae6b74cd9f36f6b8
Luke Volpe <luke.volpe@gmail.com>
2023-11-19 16:43:37 +0000

    Add property form FE

---
+++ api/models/Property.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const propertySchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  title: String,
+  address: String,
+  photos: [String],
+  description: String,
+  features: [String],
+  extraInfo: String,
+  checkIn: Number,
+  checkOut: Number,
+  maxGuests: Number,
+});
+
+const PropertyModel = mongoose.model('Property', propertySchema);
+
+module.exports = PropertyModel;
